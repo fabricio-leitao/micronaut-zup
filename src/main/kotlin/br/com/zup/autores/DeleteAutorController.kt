ae4ca26500546121bf8be2c88a778f6c06d99001
fabricio-leitao <fabricio-beco@hotmail.com>
2021-04-01 10:35:13 -0300

    Delete

---
+++ src/main/kotlin/br/com/zup/autores/DeleteAutorController.kt
@@ -0,0 +1,25 @@
+package br.com.zup.autores
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.PathVariable
+
+@Controller("/autores/{id}")
+class DeleteAutorController(val autorRepository: AutorRepository) {
+
+    @Delete
+    fun deleta(@PathVariable id: Long): HttpResponse<Any> {
+        val possivelAutor = autorRepository.findById(id)
+
+        if(possivelAutor.isEmpty){
+            return HttpResponse.notFound()
+        }
+
+//        val autor = possivelAutor.get()
+//        autorRepository.delete(autor)
+        autorRepository.deleteById(id)
+
+        return HttpResponse.ok()
+    }
+}
